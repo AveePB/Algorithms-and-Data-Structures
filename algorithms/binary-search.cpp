b3bcee5919e0c2bfa69dced1de55846a5e22be63
AveePB <94467965+AveePB@users.noreply.github.com>
2023-02-16 16:41:30 +0100

    Create binary-search.cpp

---
+++ algorithms/binary-search.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+const int MAXN = 1e3+7; 
+vector<int> v(MAXN); 
+int n{}, m{};
+
+int query(int val){
+    int l = 0, r = n;
+
+    while (r-l > 1){
+        int mid = (l+r)/2; 
+        if(v[mid] > val)
+            r = mid; 
+        else 
+            l = mid;     
+    }
+     
+    return (v[l] == val);
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0); 
+    
+    cin >> n >> m; 
+    for(int i=0; i<n; i++)
+        cin >> v[i];
+
+    for(int i=0; i<m; i++){
+        int val; 
+        cin >> val;
+
+        cout << query(val) << '\n'; 
+    }
+    
+    return 0; 
+}
