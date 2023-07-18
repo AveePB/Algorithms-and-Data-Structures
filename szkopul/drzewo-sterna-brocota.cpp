29fc26a7386a3439056f73ff933550044e1ecaa7
AveePB <94467965+AveePB@users.noreply.github.com>
2023-07-18 12:57:04 +0200

    Update drzewo-sterna-brocota.cpp

--- szkopul/drzewo-sterna-brocota.cpp
+++ szkopul/drzewo-sterna-brocota.cpp
@@ -2,43 +2,36 @@
 
 using namespace std;
 
-vector<pair<double, double>> tree_elements; 
-int target_level{}; 
+int max_lvl{};
 
-void next_element(pair<double, double> l_border_val, pair<double, double> middle_val, pair<double, double> r_border_val, int level){
-    pair<double, double> new_middle_val; 
-    
-    if(level > target_level)
-        return;
+void next_element(pair<int, int> left, pair<int, int> mid, pair<int, int> right, int curr_lvl){    
+    if(curr_lvl++ > max_lvl) return;
 
-    new_middle_val = {
-        l_border_val.first+middle_val.first, 
-        l_border_val.second + middle_val.second
+    pair<int, int> new_mid; 
+
+    new_mid = {
+        left.first + mid.first, 
+        left.second + mid.second
     }; 
     
-    next_element(l_border_val, new_middle_val, middle_val, level+1); 
+    next_element(left, new_mid, mid, curr_lvl); 
     
-    cout << middle_val.first << "/" << middle_val.second << " "; 
+    cout << mid.first << '/' << mid.second << ' '; 
     
-    new_middle_val = {
-        middle_val.first+r_border_val.first, 
-        middle_val.second+r_border_val.second
+    new_mid = {
+        mid.first + right.first, 
+        mid.second + right.second
     };
 
-    next_element(middle_val, new_middle_val, r_border_val, level+1); 
+    next_element(mid, new_mid, right, curr_lvl); 
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0); 
-    cout.tie(0); 
-    cin.tie(0); 
+int main(){
+    ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+    cin >> max_lvl;
 
-    cin >> target_level; 
-    
     cout << "0/1 "; 
     next_element({0, 1}, {1, 1}, {1, 0}, 1);
     cout << "1/0";
-
     return 0;
 }
