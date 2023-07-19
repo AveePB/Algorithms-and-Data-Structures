bdf7837086a1678da0ad373944510ca6652e160b
AveePB <94467965+AveePB@users.noreply.github.com>
2023-07-19 14:51:49 +0200

    Update spadek.cpp

--- szkopul/spadek.cpp
+++ szkopul/spadek.cpp
@@ -1,36 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const int MAXN = 1e3+7; 
 
-vector<int> v(MAXN); 
-int n{}, max_s{}, sum{};
+int n, sum=0, arr[MAXN]; 
 
-int main()
-{
-    ios_base::sync_with_stdio(0); 
-    cout.tie(0); 
-    cin.tie(0); 
-
-    cin >> n; 
+int main() {
+    ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+    cin >> n;
     
     for(int i=0; i<n; i++){
-        cin >> v[i];
-        sum += v[i]; 
+        cin >> arr[i];
+        sum += arr[i]; 
     }
-    max_s = (sum+1)/2;
-    vector<int> dp(max_s+1); 
 
-    for(int i=0; i<n; i++)
-        for(int j=max_s; j>0; j--)
-            if(v[i] <= j)
-                dp[j] = max(dp[j], dp[j-v[i]] + v[i]);
+    int max_s = (sum+1)/2, dp[max_s+1];
+    fill_n(dp, max_s+1, 0);
 
-    int i1 = max(sum-dp[max_s], dp[max_s]); 
-    int i2 = min(sum-dp[max_s], dp[max_s]);
+    for(int i=0; i<n; i++) {
+        if(arr[i] <= max_s)
+            for(int j=max_s; j>0; j--) {
+                if(arr[i] <= j) dp[j] = max(dp[j], dp[j-arr[i]] + arr[i]);
+        }
+    }
 
-    cout << i1 << " " << i2 << endl; 
+    int son1 = max(sum-dp[max_s], dp[max_s]); 
+    int son2 = min(sum-dp[max_s], dp[max_s]);
+
+    cout << son1 << ' ' << son2 << endl; 
 
     return 0;
 }
