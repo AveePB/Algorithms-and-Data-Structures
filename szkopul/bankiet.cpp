42d74d7c87baf752406aafb83450831fbbb0a89c
AveePB <94467965+AveePB@users.noreply.github.com>
2023-07-23 21:24:29 +0200

    Update bankiet.cpp

--- szkopul/bankiet.cpp
+++ szkopul/bankiet.cpp
@@ -3,35 +3,35 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
-	int n_guests=0, ans=0; 
-	vector<int> guest_nums;
+    ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+    int n_guests=0, ans=0; 
+    vector<int> guest_nums;
 
-	guest_nums.push_back(0);
-	cin >> n_guests;
+    guest_nums.push_back(0);
+    cin >> n_guests;
 
-	for (int i=0; i<n_guests; i++) {
-		int guest_num = 0;
-		cin >> guest_num;
-		guest_nums.push_back(guest_num);
-	}
+    for (int i=0; i<n_guests; i++) {
+        int guest_num = 0;
+        cin >> guest_num;
+        guest_nums.push_back(guest_num);
+    }
 
-	for (int i=1; i<=n_guests; i++) {
-		int pot_idx=i;
+    for (int i=1; i<=n_guests; i++) {
+        int pot_idx=i;
 
-		if (guest_nums[pot_idx] != 0) {
-			while (true) {
-				if (guest_nums[pot_idx] == i) {
-					guest_nums[pot_idx] = 0;
-					ans++;
-					break;
-				}
-				int new_idx = guest_nums[pot_idx];
-				guest_nums[pot_idx] = 0;
-				pot_idx = new_idx;
-			}
-		}
-	}
-	cout << ans;
-	return 0;
+        if (guest_nums[pot_idx] != 0) {
+            while (true) {
+                if (guest_nums[pot_idx] == i) {
+                    guest_nums[pot_idx] = 0;
+                    ans++;
+                    break;
+                }
+            int new_idx = guest_nums[pot_idx];
+            guest_nums[pot_idx] = 0;
+            pot_idx = new_idx;
+            }
+        }
+    }
+    cout << ans;
+    return 0;
 }
