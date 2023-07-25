36bf955313ff532fc952ab85e494b8e2ccf4a35a
AveePB <94467965+AveePB@users.noreply.github.com>
2023-07-25 16:08:00 +0200

    Create najlepsze-sumy.cpp

---
+++ szkopul/najlepsze-sumy.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+int bestSum(vector<int> vec) {
+    if (vec.size() == 0) return 0;
+    int ans = vec[0], sum = vec[0]; 
+
+    for (int i = 1; i<vec.size(); i++) {
+        if (sum < 0) 
+            sum = vec[i];
+        else 
+            sum += vec[i];
+
+        if (ans < sum) ans = sum;
+    }
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    vector<int> v;
+    int x = 0;
+
+    while (true) {
+        cin >> x;
+        if (x == 0) break;
+        v.push_back(x);
+    }
+    cout << bestSum(v);
+    return 0;
+}
