0217af00b54ff6eecbecb50bd27fd05890bbc0a4
AveePB <94467965+AveePB@users.noreply.github.com>
2023-07-26 17:51:20 +0200

    Create liczby-parzystocyfrowe.cpp

---
+++ szkopul/liczby-parzystocyfrowe.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+string to_quinary(long long n) {
+    long long divided_n = n; 
+    string quinary_n = "";
+    string correct_quinary_n = "";
+
+    while (divided_n != 0) {
+        quinary_n += to_string((divided_n%5)*2);
+        divided_n /= 5; 
+    }
+     
+    for (long long i = quinary_n.length()-1; i >= 0; i--)
+        correct_quinary_n += quinary_n[i]; 
+    
+    return correct_quinary_n;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+    long long n = 0; 
+    
+    cin >> n;
+    cout << to_quinary(n) << endl;
+     
+    return 0; 
+}
